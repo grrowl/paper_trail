a0b74d02fc255c5176b7348cabde71e8220fbec4
Tom McKenzie <tom@chillidonut.com>
2013-04-16 16:20:40 +1000

    Mass-assign :transaction_id

--- lib/paper_trail/version.rb
+++ lib/paper_trail/version.rb
@@ -3,7 +3,7 @@ class Version < ActiveRecord::Base
   has_many :version_associations, :dependent => :destroy
   
   validates_presence_of :event
-  attr_accessible :item_type, :item_id, :event, :whodunnit, :object, :object_changes
+  attr_accessible :item_type, :item_id, :event, :whodunnit, :object, :object_changes, :transaction_id
 
   def self.with_item_keys(item_type, item_id)
     where :item_type => item_type, :item_id => item_id
